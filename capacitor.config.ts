54c4ea27acaeb48f2069979f2a0597cb79251ca3
SamirC14ç <14samirc14@gmail.com>
2023-05-20 15:26:11 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'objects-3d',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
